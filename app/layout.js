07df5a43355d6006fe076cc439161df648f09d14
appwau <jesusduquesevilla@gmail.com>
2025-10-15 16:35:06 +0200

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <head>
+        <title>AppWau</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
